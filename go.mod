3bfb195991f890033058a2e3f4b0eb21fbdad185
Andruixxd31 <andruix93@gmail.com>
2023-07-03 23:59:02 -0600

    feat: starting structure for service layer

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/andruixxd31/beginner-project
 
 go 1.19
+
+require github.com/google/uuid v1.3.0 // indirect
---
+++ internal/book/book.go
@@ -0,0 +1,49 @@
+package book
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+)
+
+type Book struct {
+    Id uuid
+    Title string
+    ISBN string
+    Author string
+    Genre string
+    UpVotes int
+}
+
+type Store interface {
+    GetBook(ctx context.Context, id uuid) (Book, error)
+    CreateBook(ctx context.Context, book Book) (Book, error)
+    UpdateBook(ctx context.Context, book Book) error
+    DeleteBook(ctx context.Context, id uuid) error
+}
+
+type Service struct {
+    Store Store
+}
+
+func NewService(store Store) *Service {
+    return &Service{
+        Store: store,
+    }
+}
+
+func (s *Service) GetBook(ctx context.Context, id uuid) (Book, error){
+    return Book{}, nil
+}
+
+func (s *Service) CreateBook(ctx context.Context, book Book) (Book, error){
+    return Book{}, nil
+}
+
+func (s *Service) UpdateBook(ctx context.Context, book Book) error{
+    return nil
+}
+
+func (s *Service) DeleteBook(ctx context.Context, id uuid) error{
+    return nil
+}
