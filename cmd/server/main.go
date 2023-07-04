58001cb92a1113d5e744947f92b263c3edba8d39
Andruixxd31 <andruix93@gmail.com>
2023-07-04 14:48:07 -0600

    chore: fixed uuid declaration and added new method

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,10 +2,8 @@ package main
 
 import (
     "fmt"
-    "github.com/andruixxd31/beginner-project/internal/network"
 )
 
 func main(){
     fmt.Println("Hello World!")
-    network.Ping("dummy ip")
 }
