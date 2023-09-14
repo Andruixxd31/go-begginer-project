e369b6f60c9c5b0e5fddfc68456feb95ca944b55
adnruixxd31 <andruix93@gmail.com>
2023-09-14 17:37:59 -0600

    feat: integration test for  UpdateAccount

--- internal/database/account_test.go
+++ internal/database/account_test.go
@@ -40,4 +40,23 @@ func TestAccountTable(t *testing.T) {
         assert.Error(t, getErr)
     })
 
+    t.Run("test update account", func(t *testing.T) {
+        db, err := NewDatabase()
+        assert.NoError(t, err)
+
+        createdAccount, err := db.CreateAccount(context.Background(), account.Account{
+            Name: "John",
+        })
+        assert.NoError(t, err)
+
+        updatedAccount, updateErr := db.UpdateAccount(context.Background(), createdAccount.Id, account.Account{
+            Name: "Juan",
+        })
+        assert.NoError(t, updateErr)
+
+        newAccount, err := db.GetAccount(context.Background(), createdAccount.Id)
+        assert.NoError(t, err)
+        assert.Equal(t, newAccount.Name, updatedAccount.Name)
+    })
+
 }
